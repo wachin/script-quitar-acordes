60ab486e784fdbce86dc839305b422f2567d1edb
wachin <wachin.id@gmail.com>
2026-05-08 22:46:06 -0500

    update

--- remueve_acordes-Y-retiene-los-corchetes.py
+++ remueve_acordes-Y-retiene-los-corchetes.py
@@ -151,7 +151,7 @@ def process_files(input_folder, output_folder):
 
 # Carpetas
 input_folder = 'canciones-a-remover-acordes'
-output_folder = 'canciones-solo-letras'
+output_folder = 'canciones-solo-letras-con-corchetes'
 
 # Ejecutar
 process_files(input_folder, output_folder)
